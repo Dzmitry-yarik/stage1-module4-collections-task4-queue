f1f605b8619247e67a7354d0f7c91cc38466f32d
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-24 19:36:20 +0300

    Update ArrayDequeCreator.java

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,23 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+            ArrayDeque<Integer> qwe = new ArrayDeque<>();
+        ArrayDeque<Integer> qw = new ArrayDeque<>(firstQueue);
+        ArrayDeque<Integer> e = new ArrayDeque<>(secondQueue);
+
+        qwe.add(qw.pollFirst());
+        qwe.add(qw.pollFirst());
+        qwe.add(e.pollFirst());
+        qwe.add(e.pollFirst());
+
+        while (qw.peek() != null) {
+            qw.addLast(qwe.pollLast());
+            qwe.add(qw.pollFirst());
+            qwe.add(qw.pollFirst());
+            e.addLast(qwe.pollLast());
+            qwe.add(e.pollFirst());
+            qwe.add(e.pollFirst());
+        }
+        return qwe;
     }
 }
