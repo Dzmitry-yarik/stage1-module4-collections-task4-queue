4de01c83f88af6a65d0cd399de4fbdfefcff41a3
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-24 19:38:38 +0300

    Update PriorityQueueCreator.java

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,24 @@
 package com.epam.collections.queue;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+            firstList.addAll(secondList); //соединяем две коллекции
+
+        Collections.sort(firstList); //сортируем коллекцию
+        Collections.reverse(firstList); //переворачиваем
+
+        //переопределям компаратор (чтобы сортировал элементы в алфавитном обратном порядке
+        Comparator<String> qw = Comparator.reverseOrder();
+
+        //передвем очереди компаратор по которому нужно сортировать
+        PriorityQueue<String> queue = new PriorityQueue<>(qw);
+        queue.addAll(firstList); //передаем очереди коллекцию
+
+        return queue;
     }
 }
